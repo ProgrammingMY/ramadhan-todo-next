ce653bc0cebfa0f9d8774307c75f402a98be5e57
programmingmy <loqmanhakim74@gmail.com>
2025-03-04 09:51:08 +0800

    disable dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 import OnboardingModal from "./_components/onboarding-modal";
 import { TodoList } from "./_components/todo-list";
