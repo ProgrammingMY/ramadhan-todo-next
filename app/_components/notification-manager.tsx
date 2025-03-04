e0ef2b9564e2b16cfdc83e1e2ad7f35f0d3c92a0
programmingmy <loqmanhakim74@gmail.com>
2025-03-04 08:51:12 +0800

    check service worker

--- app/_components/notification-manager.tsx
+++ app/_components/notification-manager.tsx
@@ -43,6 +43,7 @@ export default function NotificationManager() {
         // check if service worker is supported
         if (typeof window !== "undefined" && "serviceWorker" in navigator && window.serwist !== undefined) {
             // run only in browser
+            console.log("Service worker supported");
             navigator.serviceWorker.ready.then((reg) => {
                 reg.pushManager.getSubscription().then((sub) => {
                     if (sub && !(sub.expirationTime && Date.now() > sub.expirationTime - 5 * 60 * 1000)) {
