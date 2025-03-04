b3ff719f9af113198eca690e4e7bc412331f407d
programmingmy <loqmanhakim74@gmail.com>
2025-03-04 12:07:30 +0800

    config to enable sw

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,23 @@ const withSerwist = withSerwistInit({
     { url: "/", revision },
     { url: "/messages", revision },
   ],
+  exclude: [
+    // https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1332258575
+    ({ asset }) => {
+      // Add here any file that fails pre-caching
+      const excludeList = [
+        // Default Serwist https://serwist.pages.dev/docs/next/configuring/exclude
+        /\.map$/,
+        /^manifest.*\.js$/,
+        /^server\//,
+        /^(((app-)?build-manifest|react-loadable-manifest|dynamic-css-manifest)\.json)$/,
+      ]
+      if (excludeList.some(r => r.test(asset.name))) {
+        return true
+      }
+      return false
+    },
+  ]
 });
 
 /** @type {import("next").NextConfig} */
