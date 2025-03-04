5ddc82b0ed10d588e2cd6e94809211de289a707c
programmingmy <loqmanhakim74@gmail.com>
2025-03-04 08:44:02 +0800

    notification manager

--- app/_components/notification-manager.tsx
+++ app/_components/notification-manager.tsx
@@ -91,6 +91,8 @@ export default function NotificationManager() {
                     applicationServerKey: urlBase64ToUint8Array(process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!),
                 });
 
+                console.log("Push subscription created", sub);
+
                 if (!sub) {
                     throw new Error('Failed to create push subscription');
                 }
