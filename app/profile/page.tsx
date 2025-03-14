c83591c253576622a8f63e312404b10f11c698c6
hakim <loqmanhakim74@gmail.com>
2025-03-14 12:17:31 +0800

    disable recap for anon user or not logged in user

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -79,7 +79,8 @@ export default function Profile() {
   }, []);
 
   const filteredSettings = newSettings.filter(setting =>
-    setting.id !== 'install' || !isStandalone
+    (setting.id !== 'install' || !isStandalone) &&
+    (setting.id !== 'story' || (user && !user.isAnonymous))
   );
 
   const handleLogout = () => {
